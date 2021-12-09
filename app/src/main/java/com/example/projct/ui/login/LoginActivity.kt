aab98fd962368a4701a56e011f7c5375ce69ce53
coder <saad586305@gmail.com>
2021-12-09 09:50:44 +0500

    updated

--- app/src/main/java/com/example/projct/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/projct/ui/login/LoginActivity.kt
@@ -2,7 +2,7 @@ package com.example.projct.ui.login
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+
 import android.widget.TextView
 
 import androidx.appcompat.app.AppCompatActivity
@@ -10,19 +10,20 @@ import com.example.projct.CreateAccount
 
 
 import com.example.projct.R
-import java.time.Instant
+
+
 
 class LoginActivity : AppCompatActivity() {
 
-    //private lateinit var signup:Button
+    private lateinit var signup:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-       var signup:Button=findViewById(R.id.submit_btn)
-        signup.setOnClickListener ({
-         var s= Intent(this,CreateAccount::class.java)
+        signup=findViewById(R.id.submit_btn)
+        signup.setOnClickListener {
+         val s= Intent(this,CreateAccount::class.java)
             startActivity(s)
-        })
+        }
 
     }
 }
